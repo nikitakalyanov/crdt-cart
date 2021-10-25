73af92dfba2c7b35f7da2ba7ddb0352a1da2a67a
Nikita Kalyanov <nikitakalyanov@gmail.com>
2021-10-25 03:07:08 +0300

    add go client proto

---
+++ go-crdt/crdt_client.go
@@ -0,0 +1,20 @@
+package main
+
+const concurrency = 8
+const serverEndpoint = "http://127.0.0.1:1111"
+
+func addProductToCart() {
+	// send request to serverEndpoint
+}
+
+func fillCart() {
+	for i := 0; i < 1000; i++ {
+		addProductToCart()
+	}
+}
+
+func main() {
+	for i := 0; i < concurrency; i++ {
+		go fillCart()
+	}
+}
---
+++ go-crdt/go.mod
@@ -0,0 +1,3 @@
+module github.com/nikitakalyanov/crdt_cart
+
+go 1.14
